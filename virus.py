ae7d7d55324c9efcf3613538f19deb16f8c7ed8a
Anabelee Ochoa <121758388+anabeleex@users.noreply.github.com>
2023-03-01 01:11:01 -0800

    Update virus.py

--- virus.py
+++ virus.py
@@ -34,7 +34,7 @@ psymptoms = []
 global y6
 
 def show_pdf(viruscode):
-    st.markdown(f'<embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://github.com/anabeleex/virolab-interfaz/raw/main/Fichas/{viruscode}.pdf" width="700" height="1000">', unsafe_allow_html=True)
+    st.markdown(f'<embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://github.com/anabeleex/virolab-interfaz/raw/main/Fichas/{viruscode}.pdf" width="700" height="700">', unsafe_allow_html=True)
 
 
 def NaiveBayes():
